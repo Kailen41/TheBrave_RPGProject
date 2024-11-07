04c2cd35320bd513587207d109a8210e5f16988a
Kailen41 <93565307+Kailen41@users.noreply.github.com>
2024-11-07 12:22:20 -0800

    Added assets to the project. Started working on animations for the move and idle of the character.

--- Assets/Scripts/PlayerStateMachine/Player.cs
+++ Assets/Scripts/PlayerStateMachine/Player.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region Components
+    public Animator anim {get; private set;}
+
     public PlayerStateMachine stateMachine { get; private set; }
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    #endregion
 
+    #region States
     private void Awake()
     {
         stateMachine = new PlayerStateMachine();
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
     }
+    #endregion
 
     private void Start()
     {
+        anim = GetComponentInChildren<Animator>();
+
         stateMachine.Initialize(idleState);
     }
 
--- Assets/Scripts/PlayerStateMachine/States/PlayerIdleState.cs
+++ Assets/Scripts/PlayerStateMachine/States/PlayerIdleState.cs
@@ -20,5 +20,10 @@ public class PlayerIdleState : PlayerState
     public override void Update()
     {
         base.Update();
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            stateMachine.ChangeState(player.moveState);
+        }
     }
 }
